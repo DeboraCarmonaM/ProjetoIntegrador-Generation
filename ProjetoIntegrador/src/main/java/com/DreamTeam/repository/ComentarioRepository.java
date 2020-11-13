1954f30ce7fef9ee108e7c9bf6f4769b8f3f985c
DeboraCarmonaM <deboracarmonadani@gmail.com>
2020-11-12 22:51:01 -0300

    Atualização Projeto

--- ProjetoIntegrador/src/main/java/com/DreamTeam/repository/ComentarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.DreamTeam.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.DreamTeam.model.ComentarioModel;
-
-public interface ComentarioRepository extends JpaRepository<ComentarioModel, Long>{
-
-}
